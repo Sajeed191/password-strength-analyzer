a0436105e9df0750e999b0042405ec9506f95d8e
Sajeed191 <mohammed81sajeed@gmail.com>
2026-03-06 15:38:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -273,5 +273,9 @@ def logout():
     return redirect("/login")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
