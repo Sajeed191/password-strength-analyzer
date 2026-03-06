72eb6db3d521c4a086ea41357d4bb9982ea68ca5
Sajeed191 <mohammed81sajeed@gmail.com>
2026-03-06 06:16:59 +0530

    Update strength.js

--- static/js/strength.js
+++ static/js/strength.js
@@ -7,49 +7,38 @@ password.addEventListener("input", function(){
 let value = password.value
 let score = 0
 
-if(value.length >= 8){
-score++
+if(value.length >= 8){score++}
+if(/[A-Z]/.test(value)){score++}
+if(/[0-9]/.test(value)){score++}
+if(/[@$!%*?&]/.test(value)){score++}
+
+if(score==1){
+bar.style.width="25%"
+bar.style.background="red"
+text.innerText="Weak"
 }
 
-if(/[A-Z]/.test(value)){
-score++
+else if(score==2){
+bar.style.width="50%"
+bar.style.background="orange"
+text.innerText="Medium"
 }
 
-if(/[0-9]/.test(value)){
-score++
+else if(score==3){
+bar.style.width="75%"
+bar.style.background="yellow"
+text.innerText="Strong"
 }
 
-if(/[@$!%*?&]/.test(value)){
-score++
-}
-
-if(score == 1){
-bar.style.width = "25%"
-bar.style.background = "red"
-text.innerText = "Weak"
-}
-
-else if(score == 2){
-bar.style.width = "50%"
-bar.style.background = "orange"
-text.innerText = "Medium"
-}
-
-else if(score == 3){
-bar.style.width = "75%"
-bar.style.background = "yellow"
-text.innerText = "Strong"
-}
-
-else if(score == 4){
-bar.style.width = "100%"
-bar.style.background = "green"
-text.innerText = "Very Strong"
+else if(score==4){
+bar.style.width="100%"
+bar.style.background="green"
+text.innerText="Very Strong"
 }
 
 else{
-bar.style.width = "0%"
-text.innerText = ""
+bar.style.width="0%"
+text.innerText=""
 }
 
 })
